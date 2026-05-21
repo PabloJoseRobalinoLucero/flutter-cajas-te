57820ad22fa30b24e8ecf9642fc2242ac9864f74
PabloJoseRobalinoLucero <pablorobalinotrabajo@hotmail.com>
2026-05-21 02:23:04 -0500

    ultimas modificaciones para trabajar con jdk22 en android

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -7,11 +7,11 @@ plugins {
 android {
     namespace = "com.example.cajas_te_app"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "27.0.12077973"
+    ndkVersion = "28.2.13676358"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_22
+        targetCompatibility = JavaVersion.VERSION_22
     }
 
     defaultConfig {
